d6292f90d0e1c679448ac0fcf65b3d4f70de92e3
EvansWick <ivan.maslykov2000@gmail.com>
2026-08-07 17:14:15 +0300

    Add calculateSurfaceArea

--- assets/js/indx.js
+++ assets/js/indx.js
@@ -7,3 +7,14 @@ const checkMultiplicity = function (num1, num2) {
   return num1 % num2 === 0;
 };
 console.log(checkMultiplicity(20, 8));
+
+const calculateSurfaceArea = (shape, param1, param2) => {
+  if (!+param1 || !+param2 || param1 <= 0 || param2 <= 0) return 0;
+  if (shape === "cube") {
+    return Math.ceil(6 * param1 ** 2);
+  } else if (shape === "cylinder") {
+    return Math.ceil(2 * Math.PI * param1 * (param1 + param2));
+  } else {
+    return "Incorrect shape";
+  }
+};
