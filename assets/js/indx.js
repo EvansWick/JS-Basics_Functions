b246880d68395f0e7357e5f0afc133dca23d2a64
EvansWick <ivan.maslykov2000@gmail.com>
2026-08-07 16:16:14 +0300

    Add checkMultiplicity

--- assets/js/indx.js
+++ assets/js/indx.js
@@ -2,3 +2,8 @@ function isWorkingAgePerson(age) {
   return age >= 16 && age <= 64;
 }
 console.log(isWorkingAgePerson(29));
+
+const checkMultiplicity = function (num1, num2) {
+  return num1 % num2 === 0;
+};
+console.log(checkMultiplicity(20, 8));
